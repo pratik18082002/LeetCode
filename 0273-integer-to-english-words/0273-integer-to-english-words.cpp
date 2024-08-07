f79006ee793a462aeef5c9f56656265cafcbb4c6
Pratik Jadhav <89136262+pratik18082002@users.noreply.github.com>
2024-08-07 19:44:01 +0530

    Time: 3 ms (66.96%), Space: 11.8 MB (56.92%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,62 +1,56 @@
 class Solution {
 public:
-    string numberToWords(int num) {
-        if(num==0) return "Zero";
-        vector<string> measurement={"Thousand","Million","Billion"};
+    string numberToWord(int unitsPlace, int tensPlace, int hundredsPlace){
+        string ans="";
         vector<string> digits={"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
-        vector<string> tPlace1={"Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
-        vector<string> tPlace={"","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
-        string n=to_string(num),ans="";
-        for(int i=(3-(n.size()%3))%3;i>0;i--) n="0"+n;
-        int count=0;
-        for(int i=n.size()-3;i>=0;i-=3){
-            string temp="";
-            if(i>=0){
-                if(n[i]!='0'){
-                    temp+=digits[n[i]-'0']+" Hundred";
-                    if(n[i+1]!='0'){
-                        if(n[i+1]=='1') temp+=" "+tPlace1[n[i+2]-'0'];
-                        else{
-                            temp+=" "+tPlace[n[i+1]-'0'];
-                            if(n[i+2]!='0') temp+=" "+digits[n[i+2]-'0'];
-                        } 
-                    }
-                    else{
-                        if(n[i+2]!='0') temp+=" "+digits[n[i+2]-'0'];
-                    }
-                }
+        vector<string> teen={"Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+        vector<string> tens={"","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+        if(hundredsPlace!=0){
+            ans=digits[hundredsPlace]+" Hundred";
+            if(tensPlace!=0){
+                if(tensPlace==1) ans+=" "+teen[unitsPlace];
                 else{
-                    if(n[i+1]!='0'){
-                        // cout<<"*******";
-                        if(n[i+1]=='1'){
-                            temp+=tPlace1[n[i+2]-'0'];
-                            // cout<<"*******";
-                        }
-                        else{
-                            temp+=tPlace[n[i+1]-'0'];
-                            if(n[i+2]!='0') temp+=" "+digits[n[i+2]-'0'];
-                        }
-                    }
-                    else{
-                        if(n[i+2]!='0') temp+=digits[n[i+2]-'0'];
-                        // else temp+="Zero";
-                    }
+                    ans+=" "+tens[tensPlace];
+                    if(unitsPlace!=0) ans+=" "+digits[unitsPlace];
                 }
-            }
-            if(n.size()==i+3){
-                if(temp.size()==0){
-                    continue;
-                }
-                ans+=temp;
             }
             else{
-                if(temp.size()==0){
-                    count++;
-                    continue;
+                if(unitsPlace!=0) ans+=" "+digits[unitsPlace];
+            }
+        }
+        else{
+            if(tensPlace!=0){
+                if(tensPlace==1) ans+=teen[unitsPlace];
+                else{
+                    ans+=tens[tensPlace];
+                    if(unitsPlace!=0) ans+=" "+digits[unitsPlace];
                 }
-                if(ans.size()==0) ans=temp+" "+measurement[count];
-                else ans=temp+" "+measurement[count]+" "+ans;
-                count++;
+            }
+            else{
+                if(unitsPlace!=0) ans+=digits[unitsPlace];
+            }
+        }
+        return ans;
+    }
+
+    string numberToWords(int num) {
+        if(num==0) return "Zero";
+        string ans="";
+        vector<string> measurement={"Thousand","Million","Billion"};
+        for(int i=0;i<4&&num!=0;i++){
+            int unitsPlace=num%10;
+            num/=10;
+            int tensPlace=num%10;
+            num/=10;
+            int hundredsPlace=num%10;
+            num/=10;
+            cout<<unitsPlace<<" "<<tensPlace<<" "<<hundredsPlace;
+            string temp=numberToWord(unitsPlace,tensPlace,hundredsPlace);
+            if(temp=="") continue;
+            if(i==0) ans=temp;
+            else{
+                if(ans=="") ans=temp+" "+measurement[i-1];
+                else ans=temp+" "+measurement[i-1]+" "+ans;
             }
         }
         return ans;
