3c5bfd5a20c3ee97c6abed2726cbea74ee1f3e38
Pratik Jadhav <89136262+pratik18082002@users.noreply.github.com>
2024-08-22 23:49:16 +0530

    Time: 0 ms (100%), Space: 7.5 MB (57.13%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -5,7 +5,6 @@ public:
         long long power=1;
         while(num){
             if((num&1)==0) ans+=power;
-            cout<<num<<endl;
             num=num>>1;
             power=power<<1;
         }
