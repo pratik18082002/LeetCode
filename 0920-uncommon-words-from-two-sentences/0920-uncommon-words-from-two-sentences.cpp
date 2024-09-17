ebf515dc742f06dc032f3e91623f6027fbb0aee9
Pratik Jadhav <89136262+pratik18082002@users.noreply.github.com>
2024-09-17 19:09:50 +0530

    Time: 0 ms (100%), Space: 9.6 MB (7.1%) - LeetHub

---
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> sentenceToWords(string s){
+        vector<string> words;
+        string temp;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' '){
+                words.push_back(temp);
+                temp="";
+            }
+            else temp+=s[i];
+        }
+        words.push_back(temp);
+        return words;
+    }
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        vector<string> ans;
+        map<string,int> mp;
+        vector<string> wordsOfS1(sentenceToWords(s1));
+        vector<string> wordsOfS2(sentenceToWords(s2));
+        for(string word:wordsOfS1) {
+            if(find(wordsOfS2.begin(),wordsOfS2.end(),word)==wordsOfS2.end()){
+                ans.push_back(word);
+            }
+        }
+        for(string word:wordsOfS2) {
+            if(find(wordsOfS1.begin(),wordsOfS1.end(),word)==wordsOfS1.end()){
+                ans.push_back(word);
+            }
+        }
+        for(auto w:ans) mp[w]++;
+        ans.clear();
+        for(auto w:mp) if(w.second==1) ans.push_back(w.first);
+        return ans;
+    }
+};
