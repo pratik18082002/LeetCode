e8c7bef9e89bea2e64f9b169f8bf4ca34e24e8b3
Pratik Jadhav <89136262+pratik18082002@users.noreply.github.com>
2024-09-17 20:42:44 +0530

    Time: 5 ms (15.91%), Space: 8.9 MB (45.08%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
-    vector<string> sentenceToWords(string s){
-        vector<string> words;
-        string temp;
-        for(int i=0;i<s.size();i++){
-            if(s[i]==' '){
-                words.push_back(temp);
-                temp="";
-            }
-            else temp+=s[i];
-        }
-        words.push_back(temp);
-        return words;
-    }
+    // vector<string> sentenceToWords(string s){
+    //     vector<string> words;
+    //     string temp;
+    //     for(int i=0;i<s.size();i++){
+    //         if(s[i]==' '){
+    //             words.push_back(temp);
+    //             temp="";
+    //         }
+    //         else temp+=s[i];
+    //     }
+    //     words.push_back(temp);
+    //     return words;
+    // }
     vector<string> uncommonFromSentences(string s1, string s2) {
         vector<string> ans;
         map<string,int> mp;
-        vector<string> wordsOfS1(sentenceToWords(s1));
-        vector<string> wordsOfS2(sentenceToWords(s2));
-        for(string word:wordsOfS1) {
-            if(find(wordsOfS2.begin(),wordsOfS2.end(),word)==wordsOfS2.end()){
-                ans.push_back(word);
+        s1+=" "+s2;
+        string temp="";
+        for(char c:s1){
+            if(c==' '){
+                mp[temp]++;
+                temp="";
             }
+            else temp+=c;
         }
-        for(string word:wordsOfS2) {
-            if(find(wordsOfS1.begin(),wordsOfS1.end(),word)==wordsOfS1.end()){
-                ans.push_back(word);
-            }
-        }
-        for(auto w:ans) mp[w]++;
-        ans.clear();
-        for(auto w:mp) if(w.second==1) ans.push_back(w.first);
+        mp[temp]++;
+        for(auto i:mp) if(i.second==1) ans.push_back(i.first);
         return ans;
+
+        // vector<string> ans;
+        // map<string,int> mp;
+        // vector<string> wordsOfS1(sentenceToWords(s1));
+        // vector<string> wordsOfS2(sentenceToWords(s2));
+        // for(string word:wordsOfS1) {
+        //     if(find(wordsOfS2.begin(),wordsOfS2.end(),word)==wordsOfS2.end()){
+        //         ans.push_back(word);
+        //     }
+        // }
+        // for(string word:wordsOfS2) {
+        //     if(find(wordsOfS1.begin(),wordsOfS1.end(),word)==wordsOfS1.end()){
+        //         ans.push_back(word);
+        //     }
+        // }
+        // for(auto w:ans) mp[w]++;
+        // ans.clear();
+        // for(auto w:mp) if(w.second==1) ans.push_back(w.first);
+        // return ans;
     }
 };
