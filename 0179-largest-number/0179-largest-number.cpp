ed27e1b004d44ae0923918459f123040de42fd85
Pratik Jadhav <89136262+pratik18082002@users.noreply.github.com>
2024-09-19 00:52:55 +0530

    Time: 4 ms (81.09%), Space: 15.6 MB (85.32%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-    // bool cmp(string a,string b,int i,int j) {
-    //     if(a[i]>b[j]) return true;
-    //     else if(a[i]<b[j]) return false;
-    //     else{
-    //         if(i+1==a.size()&&j+1==b.size()) return true;
-    //         else if(i+1==a.size()&&j+1!=b.size()) return cmp(a,b,i,j+1);
-    //         else if(i+1!=a.size()&&j+1==b.size()) return cmp(a,b,i+1,j);
-    //         else return cmp(a,b,i+1,j+1);
-    //     }
-    // }
-    bool static compare(string s1,string s2){ 
-        return s1+s2<s2+s1;
+    bool static cmp(int a,int b){
+        string s1=to_string(a);
+        string s2=to_string(b);
+        return s1+s2>s2+s1;
     }
     string largestNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end(),cmp);
+        if(nums[0]==0) return "0";
         string ans="";
-        vector<string> n;
-        for(int i:nums) n.push_back(to_string(i));
-        sort(n.begin(),n.end(),compare);
-        for(int i=n.size()-1;i>=0;i--){
-            cout<<n[i]<<endl;
-            ans+=n[i];
-        }
+        for(int num:nums) ans+=to_string(num);
         return ans;
     }
 };
