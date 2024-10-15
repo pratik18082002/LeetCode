641ecbb01c14f8f07ceecec5a639702d121e4d10
Pratik Jadhav <89136262+pratik18082002@users.noreply.github.com>
2024-10-15 19:12:54 +0530

    Time: 32 ms (61.26%), Space: 15.3 MB (95.37%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        int i=0,j=0,times=0;
+        int firstOne=0,firstZero=0,count=0;
         long long ans=0;
-        for(i=0;i<s.size();i++) if(s[i]=='1') break;
-        for(j=s.size()-1;j>=0;j--) if(s[j]=='0') break;
-        for(int k=i;k<=j;k++){
-            if(s[k]=='1') times++;
-            else ans+=times;
+        for(firstOne=0;firstOne<s.size();firstOne++) if(s[firstOne]=='1') break;
+        for(firstZero=s.size()-1;firstZero>=0;firstZero--) if(s[firstZero]=='0') break;
+        for(int k=firstOne;k<=firstZero;k++){
+            if(s[k]=='1') count++;
+            else ans+=count;
         }
         return ans;
     }
