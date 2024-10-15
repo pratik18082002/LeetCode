3df04025074db341e5a50e84d2a9501b5d757b39
Pratik Jadhav <89136262+pratik18082002@users.noreply.github.com>
2024-10-15 19:10:57 +0530

    Time: 32 ms (61.26%), Space: 15.6 MB (76.42%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -5,7 +5,6 @@ public:
         long long ans=0;
         for(i=0;i<s.size();i++) if(s[i]=='1') break;
         for(j=s.size()-1;j>=0;j--) if(s[j]=='0') break;
-        cout<<i<<" "<<j;
         for(int k=i;k<=j;k++){
             if(s[k]=='1') times++;
             else ans+=times;
