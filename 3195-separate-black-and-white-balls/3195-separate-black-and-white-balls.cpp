9014bea5f7093e5371473a3cffae8209f41926d6
Pratik Jadhav <89136262+pratik18082002@users.noreply.github.com>
2024-10-15 19:10:40 +0530

    Time: 28 ms (85.68%), Space: 15.6 MB (76.42%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        int i=0,j=0,times=0;
+        long long ans=0;
+        for(i=0;i<s.size();i++) if(s[i]=='1') break;
+        for(j=s.size()-1;j>=0;j--) if(s[j]=='0') break;
+        cout<<i<<" "<<j;
+        for(int k=i;k<=j;k++){
+            if(s[k]=='1') times++;
+            else ans+=times;
+        }
+        return ans;
+    }
+};
